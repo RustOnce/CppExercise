5863c45e23bd5c84776d847f39ab494fc66c094a
RustOnce <RustOnce@gmail.com>
2017-08-25 18:19:29 +0800

    convertBST added

--- common/BinaryTree.hpp
+++ common/BinaryTree.hpp
@@ -118,10 +118,81 @@ void DestructTree(TreeNode* root)
     delete root;
 }
 
+void UpdateRightNodeVal(TreeNode* node, int father_val, int father_origin);
+
+inline void UpdateLeftNodeVal(TreeNode* node, int father_val)
+{
+    node->val += father_val;
+    if (node->left)
+    {
+        node->val -= node->left->val;
+        UpdateLeftNodeVal(node->left, node->val);
+    }
+    if (node->right)
+    {
+        auto val = node->val - father_val - node->right->val;
+        UpdateRightNodeVal(node->right, node->val, val);
+    }
+}
+inline void UpdateRightNodeVal(TreeNode* node, int father_val, int father_origin)
+{
+    int val = node->val;
+    father_val -= father_origin;
+    if (node->left)
+    {
+        father_val -= node->left->val;
+        val -= node->left->val;
+        UpdateLeftNodeVal(node->left, father_val);
+    }
+    if (node->right)
+    {
+        val -= node->right->val;
+        UpdateRightNodeVal(node->right, father_val, val);
+    }
+}
 TreeNode* convertBST(TreeNode* root)
 {
     if (!root)
         return root;
+
+    std::stack<TreeNode*> s;
+    auto node = root;
+    TreeNode* last_node = nullptr;
+    s.push(node);
+    while (!s.empty())
+    {
+        node = s.top();
+        if (node->left == nullptr && node->right == nullptr || (last_node && (node->left == last_node || node->right == last_node)))
+        {
+            s.pop();
+            if (node->left)
+                node->val += node->left->val;
+            if (node->right)
+                node->val += node->right->val;
+            last_node = node;
+            node = nullptr;
+        }
+        else
+        {
+            if (node->right)
+                s.push(node->right);
+            if (node->left)
+                s.push(node->left);
+        }
+    }
+
+    if (root->left)
+    {
+        root->val -= root->left->val;
+        UpdateLeftNodeVal(root->left, root->val);
+    }
+    if (root->right)
+    {
+        auto val = root->val - root->right->val;
+        UpdateRightNodeVal(root->right, root->val, val);
+    }
+
+    return root;
 }
 
 #endif
