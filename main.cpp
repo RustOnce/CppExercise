658caa259b3ebfd23daed6569b517bd017a9d9e3
RustOnce <RustOnce@gmail.com>
2017-08-25 09:21:28 +0800

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "./common/BinaryTree.hpp"
 #include <assert.h>
 #include <chrono>
 #include <future>
@@ -7,64 +8,15 @@
 #include <thread>
 #include <vector>
 
-//2dbe109240b13818635973599c88bb58
+// 2dbe109240b13818635973599c88bb58
 
 using namespace std;
-
-typedef struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x)
-        : val(x)
-        , left(nullptr)
-        , right(nullptr){};
-} TreeNode;
-
-TreeNode* BuildBST();
-void DeStructTree(TreeNode* root);
 
 int main(void)
 {
     auto root = BuildBST();
+    PreOrderTraverse(root);
+
     getchar();
     return 0;
-}
-
-TreeNode* ConvertBST(TreeNode* root)
-{
-    if (!root)
-        return root;
-    stack<TreeNode*> s;
-}
-TreeNode* BuildBST()
-{
-    auto root = new TreeNode(56);
-    auto node = new TreeNode(28);
-    root->left = node;
-    auto temp = new TreeNode(20);
-    node->left = temp;
-    temp->right = new TreeNode(26);
-    root->left->right = new TreeNode(38);
-    root->right = new TreeNode(75);
-    node = root->right;
-    node->left = new TreeNode(62);
-    node->right = new TreeNode(100);
-    node->right->left = new TreeNode(90);
-    node->right->right = new TreeNode(130);
-
-    return root;
-}
-
-void DestructTree(TreeNode* root)
-{
-    if (!root)
-        return;
-
-    if (root->left)
-        DestructTree(root->left);
-    if (root->right)
-        DestructTree(root->right);
-
-    delete root;
 }
